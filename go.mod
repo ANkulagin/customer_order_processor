bc134c50cc3cab79cbd23dd99e798ca65178d800
AndreyKulagin <ankulskull@gmail.com>
2023-10-18 21:28:26 +0300

    "Initial commit"

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ANkulagin/customer_order_processor
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ANkulagin/customer_order_processor/interfaces/order"
+
+func main() {
+	order.New()
+}
